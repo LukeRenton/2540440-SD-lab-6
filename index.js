9f98d7216a433cfdb0dc0ae4a98d3b81f1c23a1e
Luke Renton <luke.renton@icloud.com>
2024-04-08 15:11:10 +0200

    Adapted code for local testing

--- index.js
+++ index.js
@@ -6,12 +6,12 @@ app.use(express.json());
 
 const cars = require('./cars.json');
 
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-  });
+// app.use(function(req, res, next) {
+//     res.header("Access-Control-Allow-Origin", "*");
+//     res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE");
+//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//     next();
+//   });
 
 //get all cars
 app.get('/cars', (req, res) => {
